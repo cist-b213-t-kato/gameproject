c6e28b062b00ce367e64b8a28f47df7b507a7820
katot213 <katot213@kklab.spub.chitose.ac.jp>
2018-03-21 23:04:20 +0900

    マルバツゲーム的な

--- src/marubatsu/Controller.java
+++ src/marubatsu/Controller.java
@@ -8,17 +8,51 @@ import javafx.scene.layout.GridPane;
 
 public class Controller {
 
+	private int[][] board = {
+			{ 0, 0, 0 },
+			{ 0, 0, 0 },
+			{ 0, 0, 0 }
+	};
+
+	private int turn = 1;
+
 	@FXML
 	private GridPane gridPane;
 
 	@FXML
 	private void initialize() {
-		System.out.println("nekoneko");
-		Image image = new Image("marubatsu/landolt.png");
-		ImageView imageView = new ImageView(image);
-		imageView.setFitWidth(32);
-		imageView.setFitHeight(32);
-		gridPane.add(imageView, 1, 1);
+		for ( int i=0; i<3; i++ ) {
+			for ( int j=0; j<3; j++ ) {
+				ImageView imageView = new ImageView(new Image("marubatsu/empty.gif"));
+				imageView.setFitWidth(64);
+				imageView.setFitHeight(64);
+				String posStr = i + "," + j;
+				final int m = i;
+				final int n = j;
+				imageView.setOnMouseClicked(event->{
+					System.out.println(posStr);
+					if ( turn == 1 ) {
+						board[m][n] = 1;
+						imageView.setImage(new Image("marubatsu/landolt.png"));
+					} else if ( turn == 2 ) {
+						board[m][n] = 2;
+						imageView.setImage(new Image("marubatsu/batsu.jpg"));
+					}
+					turn = turn % 2 + 1;
+				});
+				imageView.setStyle("-fx-alignment:center;");
+				gridPane.add(imageView, i, j);
+			}
+		}
+		gridPane.setGridLinesVisible(true);
+
+//		Label label = new Label("o");
+//		label.setStyle("-fx-alignment:center; -fx-font-size:64px;");
+//
+//		label.setOnMouseClicked(event->{
+//			label.setText("x");
+//		});
+//		gridPane.add(label, 1, 1);
 	}
 
 	@FXML
